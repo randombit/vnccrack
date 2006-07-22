fb4fcc0cc2f744ccfa1c8c7100f1719dc0d4b16f
Jack Lloyd <lloyd@randombit.net>
2006-07-22 06:44:18 +0000

    Fix bug in wordlist, was not returning anything in next_line()

--- wordlist.cpp
+++ wordlist.cpp
@@ -25,6 +25,7 @@ std::string Wordlist::next_line()
 
    if(next.length() > 8)
       next = next.substr(0, 8); /* truncate to 8 chars, VNC's limit */
+   return next;
    }
 
 std::string Wordlist::next()
@@ -32,10 +33,7 @@ std::string Wordlist::next()
    std::string line = next_line();
 
    while(line == last)
-      {
-      printf("Skipping %s\n", line.c_str());
       line = next_line();
-      }
 
    last = line;
    return line;
