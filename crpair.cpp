df5d2695c7b21276a92ed01803b229f10c5c213f
Jack Lloyd <lloyd@randombit.net>
2006-07-22 07:26:45 +0000

    Add hex decoding the the ChallengeResponse constructor

--- crpair.cpp
+++ crpair.cpp
@@ -3,6 +3,31 @@
  */
 
 #include "vnccrack.h"
+#include <cctype>
+
+namespace {
+
+unsigned char get_nibble(char hex)
+   {
+   if(std::isupper(hex))
+      return (hex - 'A') + 10; // upper case
+   else if(std::islower(hex))
+      return (hex - 'a') + 10; // lower case
+   else
+      return (hex - '0'); // digit
+   }
+
+void hex_decode(unsigned char out[16], const char* in)
+   {
+   for(int j = 0; j != 32; j += 2)
+      {
+      unsigned char nibble1 = get_nibble(in[j]);
+      unsigned char nibble2 = get_nibble(in[j+1]);
+      out[j/2] = (nibble1 << 4) | nibble2;
+      }
+   }
+
+}
 
 bool ChallengeResponse::is_solved() const
    {
@@ -29,4 +54,9 @@ ChallengeResponse::ChallengeResponse(const std::string& line)
 
    if(hex.size() != 64)
       throw Exception("Bad C/R input line " + line);
+
+   const char* hex_str = hex.c_str();
+
+   hex_decode(challenge, hex_str);
+   hex_decode(response, hex_str + 32);
    }
