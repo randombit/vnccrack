cc5783adcc371079b74537da85575db1fa8924df
Jack Lloyd <lloyd@randombit.net>
2008-11-07 21:16:24 +0000

    Move everything to a single file

--- vnccrack.cpp
+++ vnccrack.cpp
@@ -1,23 +1,113 @@
 /*
   (C) 2008 Jack Lloyd <lloyd@randombit.net>
 */
-#include <iostream>
-#include <string>
-#include <stdexcept>
-#include <sstream>
-#include <fstream>
 #include <cctype>
+#include <exception>
+#include <fstream>
+#include <iosfwd>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string>
+#include <vector>
 
-#include <pcap.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <arpa/inet.h>
-#include <netinet/if_ether.h>
 
-#include "vnccrack.h"
+#include <pcap.h>
 
 #include <botan/botan.h>
+#include <botan/des.h>
+
+class TrialPassword
+   {
+   public:
+      std::string password() const { return pass; }
+      const Botan::DES& des_obj() const { return des; }
+
+      TrialPassword(const std::string&);
+   private:
+      Botan::DES des;
+      std::string pass;
+   };
+
+class ChallengeResponse
+   {
+   public:
+      bool is_solved() const { return !solution.empty(); }
+      std::string solution_is() const { return solution; }
+      std::string to_string() const { return id; }
+
+      void test(const TrialPassword&);
+
+      ChallengeResponse(const std::string& challenge,
+                        const std::string& response,
+                        const std::string& id);
+   private:
+      std::string id, challenge, response, solution;
+   };
+
+class ChallengeResponses
+   {
+   public:
+      int count() const;
+      bool all_solved() const;
+
+      void add(const ChallengeResponse& cr) { crpairs.push_back(cr); }
+
+      void test(const std::string&, class Report&);
+
+      ChallengeResponses() {}
+   private:
+      std::vector<ChallengeResponse> crpairs;
+   };
+
+class Password_Source
+   {
+   public:
+      virtual bool has_more() const = 0;
+      virtual std::string next() = 0;
+      virtual ~Password_Source() {}
+   };
+
+class Wordlist : public Password_Source
+   {
+   public:
+      bool has_more() const;
+      std::string next();
+
+      Wordlist(const std::string&);
+      ~Wordlist();
+   private:
+      std::string next_line();
+
+      std::istream* in;
+      bool owns;
+      std::string last;
+   };
+
+class Report
+   {
+   public:
+      virtual void solution(const ChallengeResponse&, const std::string&) = 0;
+      virtual ~Report() {}
+   };
+
+class VNC_Cracker
+   {
+   public:
+      void crack(ChallengeResponses&);
+
+      VNC_Cracker(Report& r, Password_Source& s) :
+         report(r), source(s) {}
+   private:
+      Report& report;
+      Password_Source& source;
+   };
 
 class Packet_Reader
    {
@@ -209,6 +299,152 @@ class Cout_Report : public Report
          std::cout << "Found: " << cr << " -> " << pass << "\n";
          }
    };
+
+
+void ChallengeResponse::test(const TrialPassword& pass)
+   {
+   if(is_solved())
+      return;
+
+   const Botan::DES& des = pass.des_obj();
+
+   unsigned char encrypted_challenge[16] = { 0 };
+   des.encrypt((const Botan::byte*)&challenge[0], &encrypted_challenge[0]);
+
+   if(std::memcmp(encrypted_challenge, &response[0], 8) == 0)
+      {
+      des.encrypt((const Botan::byte*)&challenge[8], &encrypted_challenge[8]);
+
+      if(std::memcmp(encrypted_challenge, &response[0], 16) == 0)
+         solution = pass.password();
+      }
+   }
+
+ChallengeResponse::ChallengeResponse(const std::string& challenge_in,
+                                     const std::string& response_in,
+                                     const std::string& id_in)
+   {
+   challenge = challenge_in;
+   response = response_in;
+   id = id_in;
+   }
+
+void ChallengeResponses::test(const std::string& pass_str,
+                              Report& report)
+   {
+   TrialPassword pass(pass_str);
+
+   for(std::size_t j = 0; j != crpairs.size(); j++)
+      {
+      if(crpairs[j].is_solved())
+         continue;
+
+      crpairs[j].test(pass);
+
+      if(crpairs[j].is_solved())
+         report.solution(crpairs[j], pass.password());
+      }
+   }
+
+int ChallengeResponses::count() const
+   {
+   return crpairs.size();
+   }
+
+bool ChallengeResponses::all_solved() const
+   {
+   for(std::size_t j = 0; j != crpairs.size(); j++)
+      if(!crpairs[j].is_solved())
+         return false;
+   return true;
+   }
+
+TrialPassword::TrialPassword(const std::string& password) : pass(password)
+   {
+   static const unsigned char bit_flip[256] = {
+      0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0,
+      0x30, 0xB0, 0x70, 0xF0, 0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8,
+      0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 0x04, 0x84, 0x44, 0xC4,
+      0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
+      0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC,
+      0x3C, 0xBC, 0x7C, 0xFC, 0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2,
+      0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 0x0A, 0x8A, 0x4A, 0xCA,
+      0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+      0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6,
+      0x36, 0xB6, 0x76, 0xF6, 0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE,
+      0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE, 0x01, 0x81, 0x41, 0xC1,
+      0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+      0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9,
+      0x39, 0xB9, 0x79, 0xF9, 0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5,
+      0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5, 0x0D, 0x8D, 0x4D, 0xCD,
+      0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+      0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3,
+      0x33, 0xB3, 0x73, 0xF3, 0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB,
+      0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB, 0x07, 0x87, 0x47, 0xC7,
+      0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
+      0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF,
+      0x3F, 0xBF, 0x7F, 0xFF };
+
+   unsigned char pass_buf[8] = { 0 };
+   for(std::size_t j = 0; j != pass.length() && j != 8; j++)
+      pass_buf[j] = bit_flip[(unsigned char)pass[j]];
+
+   des.set_key(pass_buf, sizeof(pass_buf));
+   }
+
+void VNC_Cracker::crack(ChallengeResponses& crs)
+   {
+   while(source.has_more() && !crs.all_solved())
+      crs.test(source.next(), report);
+   }
+
+Wordlist::Wordlist(const std::string& file)
+   {
+   if(file == "-")
+      {
+      in = &std::cin;
+      owns = false;
+      }
+   else
+      {
+      in = new std::ifstream(file.c_str());
+      owns = true;
+      }
+   }
+
+Wordlist::~Wordlist()
+   {
+   if(owns)
+      delete in;
+   }
+
+bool Wordlist::has_more() const
+   {
+   return in->good() && !in->eof();
+   }
+
+std::string Wordlist::next_line()
+   {
+   if(!has_more())
+      return "";
+
+   std::string next;
+   std::getline(*in, next);
+
+   if(next.length() > 8)
+      next = next.substr(0, 8); /* truncate to 8 chars, VNC's limit */
+   return next;
+   }
+
+std::string Wordlist::next()
+   {
+   std::string line = next_line();
+
+   while(line == last)
+      line = next_line();
+
+   return (last = line);
+   }
 
 int main(int argc, char* argv[])
    {
